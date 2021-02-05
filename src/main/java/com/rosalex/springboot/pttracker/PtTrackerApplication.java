c8a075f3962e0a75860eba72e7a35208fea04381
Ralex-Dev <Paulian89>
2021-02-04 21:23:57 -0500

    Initial commit

---
+++ src/main/java/com/rosalex/springboot/pttracker/PtTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rosalex.springboot.pttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rosalex/springboot/pttracker/PtTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosalex.springboot.pttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
